c813873c09e724283d20b2b5f58ebc4d1125f49e
Christian Schnell <cs@rekoba.de>
2024-10-09 08:20:52 +0200

    removed explicit description

--- codepage-tables/codepage_lib.js
+++ codepage-tables/codepage_lib.js
@@ -6,46 +6,11 @@ import { CODEPAGE_TABLES } from './codepage_tables.js';
 
 export class Codepage
 {
-    // TODO: remove
-    static CP_DESCRIPTION = {
-        cp437: 'DOS Latin US',
-        cp737: 'DOS Greek',
-        cp775: 'DOS Baltic Rim',
-        cp850: 'DOS Latin 1',
-        cp852: 'DOS Latin 2',
-        cp855: 'DOS Cyrillic',
-        cp857: 'DOS Turkish',
-        cp858: 'ISO 8859-1',
-        cp860: 'DOS Portuguese',
-        cp861: 'DOS Icelandic',
-        cp862: 'DOS Hebrew',
-        cp863: 'DOS French Canada',
-        cp864: 'DOS Arabic',
-        cp865: 'DOS Nordic',
-        cp866: 'DOS Cyrillic Russian',
-        cp869: 'DOS Greek 2',
-        cp874: 'DOS Thai',
-        cp1250: 'Windows Central/Eastern Europe',
-        cp1251: 'Windows Cyrillic',
-        cp1252: 'Windows ANSI',
-        cp1253: 'Windows Greek',
-        cp1254: 'Windows Turkish',
-        cp1255: 'Windows Hebrew',
-        cp1256: 'Windows Arabic',
-        cp1257: 'Windows Baltic',
-        cp1258: 'Windows Vietnamese'
-    };
-
     static get_available_codepages()
     {
         const result = {};
-        for(const cp_id of Object.keys(CODEPAGE_TABLES))
-        {
-            if(!this.CP_DESCRIPTION.hasOwnProperty(cp_id))
-            {
-                throw new Error(`internal error: unknown codepage id ${cp_id}`);
-            }
-            result[cp_id] = this.CP_DESCRIPTION[cp_id];
+        for (const [cp_id, codepage] of Object.entries(CODEPAGE_TABLES)) {
+            result[cp_id] = codepage.description;
         }
         return result;
     }
@@ -58,7 +23,7 @@ export class Codepage
         }
         this.cp_id = cp_id;
         this.charmap = cp_id === 'cp437' ?
-            CODEPAGE_TABLES.cp437 :
-            CODEPAGE_TABLES.cp437.slice(0, 128) + CODEPAGE_TABLES[cp_id];
+            CODEPAGE_TABLES.cp437.charmap :
+            CODEPAGE_TABLES.cp437.charmap.slice(0, 128) + CODEPAGE_TABLES[cp_id].charmap;
     }
 }
